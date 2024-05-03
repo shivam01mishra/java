19a7f60d8090bf18cb9d04d5d38636f6f83e6348
Shivam Mishra <mishrashivam@iitgn.ac.in>
2024-05-04 00:13:46 +0530

    practicing uses of class of other file

--- mainPractice.java
+++ mainPractice.java
@@ -4,6 +4,14 @@ public class mainPractice {
         stack st = new stack();
         st.push(5);
         System.out.println(st.top());
-        
+
+        queue q=new queue();
+        q.push(7);
+        q.push(9);
+        q.printlist();
+        System.out.println(q.front());
+        q.pop();
+        q.printlist();
+
     }
 }
--- queue.java
+++ queue.java
@@ -1,3 +1,53 @@
 public class queue {
+    class node{
+        int val;
+        node next=null;
+        public node(int val)
+        {
+            this.val=val;
+        }
+    
+       };
+       node head=null;
+       node tail=null;
+       
+       public void push(int x)
+       {
+        node temp=new node(x);
+        if(head==null)
+        {
+            head=temp;
+            tail=temp;
+        }
+        else
+        {
+        tail.next=temp;
+        tail=temp.next;
+        }
+        
+       }
+       public void pop()
+       {
+        head=head.next;
+       }
+    
+       public void printlist()
+       {
+        node temp=head;
+        System.out.println("lest elements are");
+        while(temp!=null)
+        {  
+            System.out.println(temp.val+"-> ");
+            temp=temp.next;
+        }
+        
+       }
+    
+       public int front()
+        {
+            return head.val;
+         
+        }
+      
     
 }
