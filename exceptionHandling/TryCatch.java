91f3b41e4fea60dc81c93131674fbda1ca84cfb0
Shivam Mishra <mishrashivam@iitgn.ac.in>
2024-05-12 17:39:47 +0530

    first Try Catch java program

---
+++ exceptionHandling/TryCatch.java
@@ -0,0 +1,20 @@
+// package exceptionHandling;
+import java.lang.Throwable;
+import java.util.Scanner;
+public class TryCatch {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc= new Scanner(System.in);  
+        a=sc.nextInt();
+        b=sc.nextInt();
+        try{
+            System.out.println("a/b="+a/b);
+        }
+        catch(Exception e)
+        {
+            System.out.println("thise code throwing exception "+e);
+        }
+    }
+    
+}
